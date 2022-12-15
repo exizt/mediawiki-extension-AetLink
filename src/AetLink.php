50013b6dff4d62c0f632c0545d294542475b02e6
exizt <e2xist@gmail.com>
2022-12-16 02:07:33 +0900

    minor

--- src/AetLink.php
+++ src/AetLink.php
@@ -16,7 +16,15 @@ class AetLink {
 	 * '외부 링크'의 HTML을 처리하는 파서. 
 	 * 'false'를 반환해야 변경된 html이 적용된다고 함.
 	 * 
+	 * @param string &$url Link URL
+	 * @param string &$text Link text
+	 * @param string &$link New link HTML (if returning false)
+	 * @param string[] &$attribs Attributes to be applied
+	 * @param string $linkType External link type
+	 * @return bool|void True or no return value to continue or false to abort
+	 * 
 	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/LinkerMakeExternalLink
+	 * @see https://github.com/wikimedia/mediawiki/blob/master/includes/Hook/LinkerMakeExternalLinkHook.php
 	 */
 	public static function onLinkerMakeExternalLink( &$url, &$text, &$link, &$attribs, $linktype ) { 
 		# 설정 로드
@@ -49,7 +57,20 @@ class AetLink {
 	 * 문서 페이지에서 링크가 있을 때, 렌더링을 처리하는 파서.
 	 * 링크의 갯수만큼 시행된다.
 	 * 
+	 * @param LinkRenderer $linkRenderer
+	 * @param LinkTarget $target LinkTarget object that the link is pointing to
+	 * @param bool $isKnown Whether the page is known or not
+	 * @param string|HtmlArmor &$text Contents that the `<a>` tag should have; either a plain,
+	 *   unescaped string or an HtmlArmor object
+	 * @param string[] &$attribs Final HTML attributes of the `<a>` tag, after processing, in
+	 *   associative array form
+	 * @param string &$ret Value to return if your hook returns false
+	 * @return bool|void True or no return value to continue or false to abort. If you return
+	 *   true, an `<a>` element with HTML attributes $attribs and contents $html will be
+	 *   returned. If you return false, $ret will be returned.
+	 * 
 	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/HtmlPageLinkRendererEnd
+	 * @see https://github.com/wikimedia/mediawiki/blob/master/includes/linker/Hook/HtmlPageLinkRendererEndHook.php
 	 */
 	public static function onHtmlPageLinkRendererEnd( $linkRenderer, $target, $isKnown, &$text, &$attribs, &$ret ){
 		# self::debugLog('::onHtmlPageLinkRendererEnd');
@@ -61,6 +82,9 @@ class AetLink {
 
 	/**
 	 * 인터위키 링크의 타겟을 변경하는 메소드.
+	 * 
+	 * @param string[] &$attribs Final HTML attributes of the `<a>` tag, after processing, in
+	 *   associative array form
 	 */
 	private static function addInterwikiLinkTarget( &$attribs ){
 		# class 속성이 있는지 확인. 이게 없으면 뭔가 비교하기가 어려우므로...
@@ -106,7 +130,7 @@ class AetLink {
 		];
 
 		# 설정값 병합
-		$userSettings = self::getUserLocalSettings();
+		$userSettings = self::readSettings();
 		if (isset($userSettings)){
 			# 만약을 위한 설정값 타입 체크.
 			foreach ($userSettings as $key => $value) {
@@ -126,36 +150,38 @@ class AetLink {
 	}
 
 	/**
-	 * 설정값 조회
+	 * 전역 설정값 조회
+	 * 
+	 * @return array|null 설정된 값 또는 undefined|null를 반환
 	 */
-	private static function getUserLocalSettings(){
+	private static function readSettings(){
 		global $wgAetLink;
 		return $wgAetLink;
 	}
 
 	/**
 	 * 디버그 로깅 관련
+	 * 
+	 * @param string|object $msg 디버깅 메시지 or 오브젝트
 	 */
 	private static function debugLog($msg){
 		global $wgDebugToolbar;
 
 		# 디버그툴바 사용중일 때만 허용.
-		$useDebugToolbar = $wgDebugToolbar ?? false;
-		if( !$useDebugToolbar ){
-			return false;
+		$isDebugToolbarEnabled = $wgDebugToolbar ?? false;
+		if( !$isDebugToolbarEnabled ){
+			return;
 		}
 		
 		# 로깅
-		$userSettings = self::getUserLocalSettings();
-		$isDebug = $userSettings['debug'] ?? false;
+		$settings = self::readSettings() ?? [];
+		$isDebug = $settings['debug'] ?? false;
 		if($isDebug){
 			if(is_string($msg)){
 				wfDebugLog(static::class, $msg);
 			} else {
 				wfDebugLog(static::class, json_encode($msg));
 			}
-		} else {
-			return false;
 		}
 	}
 }
